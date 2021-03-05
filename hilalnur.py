866e2c741ad52c8e2dfbca7852a23296ecbd91e4
hilalozgur <56309191+hilalozgur@users.noreply.github.com>
2021-03-05 00:01:31 -0500

    Update hilalnur.py

--- hilalnur.py
+++ hilalnur.py
@@ -5,10 +5,5 @@ print("ikinci hilalden remote denemesi")
 
 print("hilal branchi icin deneme")
 print("ikinci hilalden remote denemesi")
-print("hilal branchi icin deneme")
-print("ikinci hilalden remote denemesi")
-print("hilal branchi icin deneme")
-print("ikinci hilalden remote denemesi")
-print("hilal branchi icin deneme")
-print("ikinci hilalden remote denemesi")
+
 
