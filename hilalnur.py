cb83c24189b4bfc2843e615ab0b3003736075d17
hilalozgur <56309191+hilalozgur@users.noreply.github.com>
2021-03-04 23:59:46 -0500

    Update hilalnur.py

--- hilalnur.py
+++ hilalnur.py
@@ -1,3 +1,14 @@
 print("hello nur")
 
 print("hilal branchi icin deneme")
+
+print("hello nur")
+print("hello nur")
+print("hello nur")
+print("hello nur")
+print("hello nur")
+print("hello nur")
+print("hello nur")
+
+
+print("hello nur")
